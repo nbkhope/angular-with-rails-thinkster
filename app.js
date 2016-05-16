25fb79b83b93a984790cde1eed30b836192e38d9
nbkhope <ryumemaru@yahoo.com>
2016-05-15 21:36:35 -0700

    Modify posts variable to become an array of post objects

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ function($scope) {
 	// in the HTML side ;) => refer to it using {{test}}
 	$scope.test = "What is up?!";
 
-	// An array of posts
+	// An array of post objects
 	$scope.posts = [
-		'post 1',
-	  'post 2',
-	  'post 3',
-	  'post 4',
-	  'post 5'
-	];
+		{ title: 'post 1', upvotes: 5},
+	  { title: 'post 2', upvotes: 2},
+	  { title: 'post 3', upvotes: 15},
+	  { title: 'post 4', upvotes: 9},
+	  { title: 'post 5', upvotes: 4},
+ 	];
 }]);
 
 /* Notes
