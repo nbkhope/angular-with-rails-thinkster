355e55af56c7d431993f6c61c337fdbbe3c3216c
nbkhope <ryumemaru@yahoo.com>
2016-05-15 21:30:48 -0700

    Add .posts variable

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope) {
+	// A test variable that will be available to the MainCtrl
+	// in the HTML side ;) => refer to it using {{test}}
 	$scope.test = "What is up?!";
+
+	// An array of posts
+	$scope.posts = [
+		'post 1',
+	  'post 2',
+	  'post 3',
+	  'post 4',
+	  'post 5'
+	];
 }]);
