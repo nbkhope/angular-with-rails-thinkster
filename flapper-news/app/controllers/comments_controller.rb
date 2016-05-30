700b3f26aa47ed62f8a8d93182da018bf239a82b
nbkhope <ryumemaru@yahoo.com>
2016-05-29 18:59:25 -0700

    Add CommentsController

---
+++ flapper-news/app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+	def create
+		post = Post.find(params[:post_id])
+		comment = post.comments.create(comment_params)
+
+		# We respond with both post and comments in CommentsController because we 
+		# are using a nested resource, although only the last object is returned 
+	  # when responding to json.
+		respond_with post, comment
+	end
+
+	def upvote
+		post = Post.find(params[:post_id])
+		comment = post.comments.find(params[:id])
+		comment.increment!(:upvotes)
+
+		respond_with post, comment
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
+end
