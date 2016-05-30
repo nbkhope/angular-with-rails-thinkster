15ff9d3ff7f7e27e1ddab6c3467fd2ae7b219d54
nbkhope <ryumemaru@yahoo.com>
2016-05-29 18:46:00 -0700

    Redefine as_json in Post model to include comments in the json

--- flapper-news/app/models/post.rb
+++ flapper-news/app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
+	# override the as_json method so that all json representations
+	# of our posts will include the comments.
+	def as_json(options = {})
+		super(options.merge(include: :comments))
+	end
 end
