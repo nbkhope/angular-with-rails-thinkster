aaba5d3bb8ea0d6d08685d1d264088c148c7f478
nbkhope <ryumemaru@yahoo.com>
2016-05-29 18:56:59 -0700

    Update ApplicationController to respond_to :json

--- flapper-news/app/controllers/application_controller.rb
+++ flapper-news/app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # in order for our controllers to respond in the json format,
+  respond_to :json
+
   def angular
   	render 'layouts/application'
   end
